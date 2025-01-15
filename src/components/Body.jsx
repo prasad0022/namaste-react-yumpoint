1f6b5a8277f208e5eeefad98ff1ad256c13123e3
Prasad <ppkhose6@gmail.com>
2025-01-15 17:45:12 +0530

    Added login/logout UI feature using useState()

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -10,14 +10,18 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING#"
-    );
-    const jsonData = await data.json();
-    setRestaurantsList(
-      jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
-        ?.restaurants
-    );
+    try {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING#"
+      );
+      const jsonData = await data.json();
+      setRestaurantsList(
+        jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { LOGO_URI } from "../utils/constants";
 
 const Header = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked((preVal) => {
+      return !preVal;
+    });
+  };
+
   return (
     <div className="header">
       <div className="company-logo">
@@ -12,8 +20,10 @@ const Header = () => {
           <li>Home</li>
           <li>About Us</li>
           <li>Contact Us</li>
-          <li>Sign In</li>
           <li>Cart</li>
+          <button className="login-btn" onClick={handleClick}>
+            {isClicked ? "Logout" : "Login"}
+          </button>
         </ul>
       </div>
     </div>
