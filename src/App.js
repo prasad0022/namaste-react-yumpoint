0b429e6c4ccedcade11f5feee4726eaf6c71dc0d
Prasad <ppkhose6@gmail.com>
2025-01-18 12:44:41 +0530

    Integrated React Router v7: Added routing setup using createBrowserRouter and RouterProvider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import ErrorPage from "./components/ErrorPage";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 const App = () => {
     return (
@@ -12,6 +16,22 @@ const App = () => {
     )
 };
 
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: "/about",
+        element: <About />
+    },
+    {
+        path: "/contact",
+        element: <Contact />
+    }
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useRouteError } from "react-router";
+
+const ErrorPage = () => {
+  const err = useRouteError();
+
+  console.log(err);
+
+  return (
+    <div className="error-container">
+      <h1>Oops !!!</h1>
+      <h2>Error Status : {err.status}</h2>
+      <h2>Error Message : {err.statusText}</h2>
+    </div>
+  );
+};
+
+export default ErrorPage;
