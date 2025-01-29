88bf3af674e1e421c13177ded21c4f15327af31e
Prasad <ppkhose6@gmail.com>
2025-01-29 18:05:38 +0530

    Implemented menu component for each categories

--- src/components/ResCategory.jsx
+++ src/components/ResCategory.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import ResMenu from "./ResMenu";
 
 const ResCategory = ({ data }) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="menu-category">
-      <div className="category-header">
+      <div className="category-header" onClick={handleClick}>
         <h1>{`${data.title} (${data.itemCards.length})`}</h1>
         <p>⬇️</p>
       </div>
+      {showMenu && (
+        <div className="menu-body">
+          {data.itemCards.map((data) => (
+            <ResMenu key={data.card.info.id} menu={data.card.info} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/ResMenu.jsx
+++ src/components/ResMenu.jsx
@@ -3,12 +3,18 @@ import React from "react";
 const ResMenu = ({ menu }) => {
   return (
     <div>
-      <hr style={{ marginTop: "30px" }} />
+      <hr />
       <div className="res-menu">
-        {menu.isVeg ? <p>🟢</p> : <p>🔴</p>}
-        <h2>{menu.name}</h2>
-        {menu.price && <h3>₹ {menu.price / 100}</h3>}
-        <p>{menu.category}</p>
+        {menu.isVeg ? (
+          <p style={{ fontSize: "10px" }}>🟢</p>
+        ) : (
+          <p style={{ fontSize: "10px" }}>🔴</p>
+        )}
+        <h2 style={{ fontSize: "16px", fontWeight: "500" }}>{menu.name}</h2>
+        {menu.price && (
+          <h3 style={{ fontSize: "12px" }}>₹ {menu.price / 100}</h3>
+        )}
+        <p style={{ fontSize: "12px" }}>{menu.category}</p>
       </div>
     </div>
   );
